2b73d37916d56154a4d2fb1d3388ea37bcd28e7c
David Serrano Indiano <dserrano@dam1-pc00.(none)>
2014-03-19 13:34:26 +0100

    car

--- 04_coche/coche.cpp
+++ 04_coche/coche.cpp
@@ -1,1 +1,29 @@
-/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+enum opciones{acelerar=1, frenar, freno_mano };
+
+enum opciones a;
+
+switch(a){
+    case 1: 
+	printf("ACELERO");
+   
+	break;
+
+    case 2:
+	printf("FRENO");
+	break;
+	
+    case 3:
+	printf("FRENO DE MANOOOO!");
+	break;
+}
+
+
+
+
+    return EXIT_SUCCESS;
+}
